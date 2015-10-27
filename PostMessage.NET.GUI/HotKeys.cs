667feb02ad8d834feebdf7e678ce5e148aeea280
Zhai YiMing <tinymins@gmail.com>
2015-10-27 16:57:08 +0800

    Import Hotkeys Class.

---
+++ PostMessage.NET.GUI/HotKeys.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace PostMessage.NET.GUI
+{
+    class HotKeys
+    {
+        // Import API
+        [DllImport("user32.dll")]
+        static extern bool RegisterHotKey(IntPtr hWnd, int id, int modifiers, Keys vk);
+        [DllImport("user32.dll")]
+        static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        // Composite keys
+        public enum HotkeyModifiers
+        {
+            Alt     = 1,
+            Control = 2,
+            Shift   = 4,
+            Win     = 8,
+        }
+
+        int keyid = 10;
+        public delegate void HotKeyCallBackHanlder();
+        Dictionary<int, HotKeyCallBackHanlder> keymap = new Dictionary<int, HotKeyCallBackHanlder>();
+        // Register HotKeys
+        public void Regist(IntPtr hWnd, int modifiers, Keys vk, HotKeyCallBackHanlder callBack)
+        {
+            int id = keyid++;
+            if (!RegisterHotKey(hWnd, id, modifiers, vk))
+                throw new Exception("Failed to register hotkeys!");
+            keymap[id] = callBack;
+        }
+
+        // Unregister HotKeys
+        public void UnRegist(IntPtr hWnd, HotKeyCallBackHanlder callBack)
+        {
+            foreach (KeyValuePair<int, HotKeyCallBackHanlder> var in keymap)
+            {
+                if (var.Value == callBack)
+                {
+                    UnregisterHotKey(hWnd, var.Key);
+                    return;
+                }
+            }
+        }
+
+        // HotKeys Event Handle
+        public void ProcessHotKey(Message m)
+        {
+            if (m.Msg == 0x312)
+            {
+                int id = m.WParam.ToInt32();
+                HotKeyCallBackHanlder callback;
+                if (keymap.TryGetValue(id, out callback))
+                    callback();
+            }
+        }
+    }
+}
