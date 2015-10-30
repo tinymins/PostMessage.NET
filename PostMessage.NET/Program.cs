6af1881d5075fd0d1c2dfd0a3759f264e04644ac
Zhai YiMing <tinymins@gmail.com>
2015-10-31 02:53:37 +0800

    Add some more param support.

--- PostMessage.NET/Program.cs
+++ PostMessage.NET/Program.cs
@@ -12,30 +12,55 @@ partial class Program {
     }
     static void Main(string[] args) {
         // args = new string[] { "31212", "54,2000,2000" };
+        // args = new string[] { "--help" };
+        bool bEcho = false;
+        bool bActive = false;
+        int nProcessId = 0;
+        string[] list = null;
         // Check arguments
-        if (args.Length == 1 && args[0] == "--help") {
-            Console.WriteLine("Usage:");
-            Console.WriteLine("    PostMessage.exe <PID> <keylist>");
-            Console.WriteLine("      <PID>     Target Process ID");
-            Console.WriteLine("      <keylist> The key list which will send to the target process.");
-            Console.WriteLine("");
-            Console.WriteLine("Ex:");
-            Console.WriteLine("    PostMessage.exe 13123 32,50,200;49,200,200");
-            Console.WriteLine("      This command will start a loop to send keylist to process 13123.");
-            Console.WriteLine("      The keylist loop is PRESS SPACE_KEY(32) for 50ms and then wait for");
-            Console.WriteLine("      200ms, and PRESS KEY_1(49) for 200ms and then wait for 200ms.");
-            Console.WriteLine("");
-            Console.WriteLine("Source code:");
-            Console.WriteLine("    https://github.com/tinymins/PostMessage.NET");
-            return;
-        } else if (args.Length != 2) {
+        int argc = args.Length;
+        for (int i = 0; i < argc; i++)
+        {
+            switch (args[i])
+            {
+                case "--help":
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("    PostMessage.exe <PID> <keylist>");
+                    Console.WriteLine("      <PID>     Target Process ID");
+                    Console.WriteLine("      <keylist> The key list which will send to the target process.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Ex:");
+                    Console.WriteLine("    PostMessage.exe 13123 32,50,200;49,200,200");
+                    Console.WriteLine("      This command will start a loop to send keylist to process 13123.");
+                    Console.WriteLine("      The keylist loop is PRESS SPACE_KEY(32) for 50ms and then wait for");
+                    Console.WriteLine("      200ms, and PRESS KEY_1(49) for 200ms and then wait for 200ms.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Source code:");
+                    Console.WriteLine("    https://github.com/tinymins/PostMessage.NET");
+                    return;
+
+                case "--echo":
+                    bEcho = true;
+                    break;
+
+                case "--active":
+                    bActive = true;
+                    break;
+
+                default:
+                    nProcessId = int.Parse(args[i]);
+                    list = args[i + 1].Split(';');
+                    i = i + 2;
+                    break;
+            }
+        }
+        if (list == null || nProcessId == 0)
+        {
             Console.WriteLine("Wrong numbers of arguments! 2 expected, got " + args.Length + "!");
             Console.WriteLine("Command --help to view more information.");
             return;
         }
         // Parse keylist
-        int nProcessId = int.Parse(args[0]);
-        string[] list = args[1].Split(';');
         Key[] keylist = new Key[list.Length];
         for (int i = 0; i < list.Length; i++) {
             if (list[i] == "") continue;
@@ -55,15 +80,20 @@ partial class Program {
             IntPtr h = p.MainWindowHandle;
             while (true) {
                 foreach (Key key in keylist) {
-                    PostMessage(h, WM_ACTIVATEAPP, 1, 0);
+                    if (bActive)
+                        PostMessage(h, WM_ACTIVATEAPP, 1, 0);
                     PostMessage(h, WM_KEYDOWN, key.nKCode, 0);
-                    Console.WriteLine("Process \"" + p.ProcessName + "\" post message KEYDOWN with key code " + key.nKCode + ".");
+                    if (bEcho)
+                        Console.WriteLine("Process \"" + p.ProcessName + "\" post message KEYDOWN with key code " + key.nKCode + ".");
                     System.Threading.Thread.Sleep(key.nPress);
-                    Console.WriteLine("Process \"" + p.ProcessName + "\" KEYPRESSING for " + key.nPress + "ms.");
+                    if (bEcho)
+                        Console.WriteLine("Process \"" + p.ProcessName + "\" KEYPRESSING for " + key.nPress + "ms.");
                     PostMessage(h, WM_KEYUP, key.nKCode, 0);
-                    Console.WriteLine("Process \"" + p.ProcessName + "\" post message KEYUP with key code " + key.nKCode + ".");
+                    if (bEcho)
+                        Console.WriteLine("Process \"" + p.ProcessName + "\" post message KEYUP with key code " + key.nKCode + ".");
                     System.Threading.Thread.Sleep(key.nDelay);
-                    Console.WriteLine("Process \"" + p.ProcessName + "\" DELAYING for " + key.nDelay + "ms.");
+                    if (bEcho)
+                        Console.WriteLine("Process \"" + p.ProcessName + "\" DELAYING for " + key.nDelay + "ms.");
                 }
             }
         } catch (Exception ex) {
