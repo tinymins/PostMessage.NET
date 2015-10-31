a62029f9fd0f3b303d64f69067ae8c19405dd542
Zhai YiMing <tinymins@gmail.com>
2015-11-01 00:26:09 +0800

    Avoid multi running app instance.

--- PostMessage.NET.WPF/App.xaml.cs
+++ PostMessage.NET.WPF/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 
@@ -12,5 +13,42 @@ namespace PostMessage.NET.WPF
     /// </summary>
     public partial class App : Application
     {
+        // Avoid multi running.
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        private const int SW_SHOWNOMAL = 1;
+        private static void HandleRunningInstance(Process instance)
+        {
+            ShowWindowAsync(instance.MainWindowHandle, SW_SHOWNOMAL);
+            SetForegroundWindow(instance.MainWindowHandle);
+        }
+        private static Process GetRuningInstance()
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            Process[] Processes = Process.GetProcessesByName(currentProcess.ProcessName);
+            foreach (Process process in Processes)
+            {
+                if (process.Id != currentProcess.Id)
+                {
+                    if (System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == currentProcess.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        App()
+        {
+            Process process = GetRuningInstance();
+            if (process != null)
+            {
+                HandleRunningInstance(process);
+                System.Environment.Exit(1);
+            }
+        }
     }
 }
